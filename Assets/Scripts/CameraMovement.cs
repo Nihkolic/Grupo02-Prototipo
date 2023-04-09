7d67c36947429ebb1612ee1add3b30148f6cc3d7
AlbertoPonce <38371708+Nihkolic@users.noreply.github.com>
2023-04-09 16:03:14 -0500

    added camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float sensitivity = 500f; 
+    public Transform player;
+    public Transform orientation;
+
+    float xRotation = 0.0f;
+    float yRotation = 0.0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;//move later
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; //rotate x axis
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f); // clamp camera rotation up or down
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0.0f);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0); 
+    }
+}
